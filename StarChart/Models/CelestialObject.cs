d42310ddf77a4c1cc8428a3ffdc1902440daa484
tylersatter <tsman13@gmail.com>
2019-11-12 18:51:56 -0800

    Added CelestialObject

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int? OrbitedObjectId { get; set; }
+
+        [NotMapped]
+        public List<CelestialObject> Satellites { get; set; }
+
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+    
+}
